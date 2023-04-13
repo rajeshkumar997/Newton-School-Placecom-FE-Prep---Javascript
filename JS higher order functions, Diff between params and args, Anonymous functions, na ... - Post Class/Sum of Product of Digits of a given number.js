3096f777ea2106aa61a0d656f9793ed2b44256f8
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-04-13 18:58:50 +0530

    Create Sum of Product of Digits of a given number.js

---
+++ JS higher order functions, Diff between params and args, Anonymous functions, na ... - Post Class/Sum of Product of Digits of a given number.js
@@ -0,0 +1,11 @@
+function sumOfProductOfDigits(n1, n2) {
+    let sum = 0;
+    while (n1 > 0 && n2 > 0) {
+        let digit1 = n1 % 10;
+        let digit2 = n2 % 10;
+        sum += digit1 * digit2;
+        n1 = Math.floor(n1 / 10);
+        n2 = Math.floor(n2 / 10);
+    }
+    return sum;
+}
